0d76f7b9f4621475e483180310bf3cf87eb40903
Syed Faizan <fyzanhussain@gmail.com>
2025-07-12 22:38:02 +0530

    VariablesAndDatatypePractise

---
+++ variablespractise.py
@@ -0,0 +1,34 @@
+# wap to find rremainder when a number is dvided by z.
+
+a=float(input("Enter 1 number : "))
+z=float(input("Enter 1 number : "))
+
+c=a%z
+print(c)
+
+
+# check the types of variables assigned using input()function
+a=input("1: ")
+b=int(input("2: "))
+c=float(input("3: "))
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+
+# use comparison operator  to find out whether a given varible a is greater than b or not take a=34 and b=80
+
+a=34
+b=80
+c=a>b
+print(c)
+
+
+# wAP to calculate the square of a number entered by user 
+a=int(input("Enter number : "))
+b=a*a
+print(f"the Square of {a} is {b}") #--> f-string method
+print("the square of a is ",b)  #--> normal method 
+
+
