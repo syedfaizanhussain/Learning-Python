59f93bf99b10e55bda5ecb70fe7c6fe57de94b17
Syed Faizan <fyzanhussain@gmail.com>
2025-07-12 21:45:10 +0530

    sum,avg,mul of 2numbers

---
+++ testing.py
@@ -0,0 +1,11 @@
+# as iam new so making sum of number
+a = float(input("Enter number a : " ))
+b = float(input("Enter number b : " ))
+sum = a+b
+avg = sum / 2.0
+mul = a*b
+check = a>b
+# print("sum is : ",sum)
+# print("Average  is : ",avg)
+# print("Multiplication is : ",mul)
+print(check)
