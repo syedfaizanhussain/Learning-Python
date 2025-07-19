20e1a0103b22084fd5112dfd5e70958d3f515239
Syed Faizan <fyzanhussain@gmail.com>
2025-07-19 19:22:46 +0530

    Break , continue , pass statements

---
+++ break,continue,pass.py
@@ -0,0 +1,42 @@
+'''
+break : 
+        break the iteration in the loops.
+'''
+# for i in range(1,21):
+#     print(i)
+#     if(i==10):
+#         break
+
+
+
+'''
+continue :
+       Skip the rest of the code in the loop and go to the next value.'''
+# for i in range(1,21):
+#     if(i==10):
+#         continue
+#     print(i)
+
+
+
+# data={
+#     "fyzan": 20,
+#     "zubair":21,
+#     "rizwan":19
+# }
+# for key ,value in data.items():  # useing break adn continue in for loops with dict!
+#     # print(data[key])
+#     # print(value)
+#     if(value==21):
+#         continue
+#     if(key=="rizwan"):
+#         break
+#     print(key,":",value)
+
+
+'''
+PASS : 
+        Pass is a  null statement in python. it instructs to do nothing!
+'''
+for i in range(0,11):
+    pass      #pass allow us to write logic or statements in future as when we want to write! it not throw an intentation error!
